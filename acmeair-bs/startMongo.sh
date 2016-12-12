e7f3f101acb743f65a8270528558058a32c8760b
cwz <517364893@qq.com>
2016-12-12 11:13:49 +0800

    dockerfile

--- acmeair-bs/startMongo.sh
+++ acmeair-bs/startMongo.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 cd /software/mongodb-3.0.7/bin
 ./mongod -f /docker/mongodb/mongodb.conf &
-sleep 20s
 MongoDB='./mongo 127.0.0.1:27017'
 $MongoDB << EOF
 use acmeair_bookingdb
 exit;
 EOF
+while [ $? != 0 ]
+do
+   MongoDB='./mongo 127.0.0.1:27017'
+   $MongoDB << EOF
+   use acmeair_bookingdb
+   exit;
+EOF
+done
 /opt/ibm/wlp/bin/server run defaultServer
