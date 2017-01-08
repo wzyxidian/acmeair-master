a99b4c04eecf73691d87a6a6d22d3c848dfb7ae4
wzyxidian <wzyxidian@gmail.com>
2017-01-08 20:58:37 +0800

    恢复到1月6号晚上版本，继续进行调整6，修改将user-agent改为User-Agent

--- acmeair-mainapp/src/main/java/com/acmeair/config/HttpRequest.java
+++ acmeair-mainapp/src/main/java/com/acmeair/config/HttpRequest.java
@@ -13,7 +13,7 @@ import java.util.Map;
 public class HttpRequest {
 
     private static String param = "uid0@email.com";
-    private static String url = "http://172.18.0.1/customer/acmeair-cs/rest/api/customer/byid/" + param;
+    private static String url = "http://192.168.0.190:81/rest/api/customer/byid/" + param;
 
 
     public static String sendGet() {
