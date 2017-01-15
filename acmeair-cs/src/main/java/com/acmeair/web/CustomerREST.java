ba0aaa40ce8ddebf12242fa4ebf7c2faba41b979
wzyxidian <wzyxidian@gmail.com>
2017-01-15 21:16:00 +0800

    并发用户数：100 请求参数	：1 核数：3 数据库服务器核数：5 程序复杂度：10 0000 并发线程数：40 数据库输入参数：1 数据库并发连接数：2 0000 表记录条数：1000 0000

--- acmeair-cs/src/main/java/com/acmeair/web/CustomerREST.java
+++ acmeair-cs/src/main/java/com/acmeair/web/CustomerREST.java
@@ -80,11 +80,11 @@ public class CustomerREST {
         private String customerid; //customer id
         private String sendtime; //请求发送时间
         private String username; //用户名
-        private int ti = 100; //数据库输入数据
+        private int ti = 1; //数据库输入数据
         private int nr = 10000000; //数据库表记录条数
-        private int z = 200000; //数据库并发连接数
-        private int to = 100; //数据库输出数据
-        private int fp = 300000; //程序复杂度
+        private int z = 20000; //数据库并发连接数
+        private int to = 1; //数据库输出数据
+        private int fp = 100000; //程序复杂度
         private long t3;
         private String[] s1;
 
