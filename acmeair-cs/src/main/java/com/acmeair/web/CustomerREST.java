b28c6d7a37f2147996e472d68a26a127447b6a74
ChinaWJB <chinawjb@foxmail.com>
2017-01-19 19:46:09 +0800

    并发用户数：5 请求参数	：1 核数	：1 数据库服务器核数：1 程序复杂度	：10000 并发线程数	：5 数据库输入参数	：10 数据库并发连接数	：2000 表记录条数	：100000

--- acmeair-cs/src/main/java/com/acmeair/web/CustomerREST.java
+++ acmeair-cs/src/main/java/com/acmeair/web/CustomerREST.java
@@ -81,11 +81,11 @@ public class CustomerREST {
         private String customerid; //customer id
         private String sendtime; //请求发送时间
         private String username; //用户名
-        private int ti = 100; //数据库输入数据
-        private int nr = 10000; //数据库表记录条数
+        private int ti = 10; //数据库输入数据
+        private int nr = 100000; //数据库表记录条数
         private int z = 2000; //数据库并发连接数
-        private int to = 100; //数据库输出数据
-        private int fp = 100000; //程序复杂度
+        private int to = 10; //数据库输出数据
+        private int fp = 10000; //程序复杂度
         private long t3;
         private String[] s1;
 
